7c8fce48c7b1277e7d02c85dc955361c2f7082f1
devpatelwd <pdev4855@gmail.com>
2026-04-05 22:07:03 +0530

    fixed allow origin problem

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173",
-                     "gym-management-v2.vercel.app"],
+    allow_origins = ["*"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
